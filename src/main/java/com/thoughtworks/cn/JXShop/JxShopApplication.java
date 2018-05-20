e8944e850f9e2292880c30ea49f6e9332666bfb6
DQing <15829258737@163.com>
2018-05-20 19:25:35 +0800

    init project
    Signed-off-by: DQing <15829258737@163.com>

---
+++ src/main/java/com/thoughtworks/cn/JXShop/JxShopApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.cn.JXShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JxShopApplication.class, args);
+	}
+}
