5e2e4a5bada871c43ad494d68ab713c4862bf66b
DQing <15829258737@163.com>
2018-05-26 22:37:18 +0800

    add until
    Signed-off-by: DQing <15829258737@163.com>

---
+++ src/main/java/com/thoughtworks/cn/JXShop/utils/LogisticsStatus.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.cn.JXShop.utils;
+
+public enum LogisticsStatus {
+    readyToShip,
+    shipping,
+    signed
+}
---
+++ src/main/java/com/thoughtworks/cn/JXShop/utils/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.thoughtworks.cn.JXShop.utils;
+
+public enum OrderStatus {
+    unPaid, paid, withdrawn, finished
+}
